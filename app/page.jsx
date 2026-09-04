502f19706e6ee57609260a7177179c4ca61e88f9
ritesh-sarkar <75468141+ritesh-sarkar@users.noreply.github.com>
2026-09-04 22:23:19 +0600

    Refactored themeShowcase

--- app/page.jsx
+++ app/page.jsx
@@ -6,6 +6,7 @@ import React from "react";
 import { Hero } from "@/components/Hero";
 import { HowItWorks } from "@/components/HowItWorks";
 import { FeaturesGrid } from '@/components/FeaturesGrid';
+import { ThemeShowcase } from "@/components/ThemeShowcase";
 
 export default function LandingPage() {
   return (
@@ -28,6 +29,7 @@ export default function LandingPage() {
         <Hero />
         <HowItWorks />
         <FeaturesGrid />
+        <ThemeShowcase />
       </main>
     </div>
   );
